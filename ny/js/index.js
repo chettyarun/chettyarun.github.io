8c71c45dd994758a0e6da5410ce7fa502641b85a
chettyarun <chetty.arun.nani@gmail.com>
2015-04-30 23:09:57 +0530

    Ny.in
    added ny folder using single page CSS navigation

---
+++ ny/js/index.js
@@ -0,0 +1,7 @@
+/*
+@author Alberto Hartzet albertohartzet.com
+*
+*I wouldn't mind if you use this piece of code in your project as long as you give credit with a link to this pen.
+*
+Licence (CC BY-NC-SA 4.0) http://creativecommons.org/licenses/by-nc-sa/4.0/
+*/
